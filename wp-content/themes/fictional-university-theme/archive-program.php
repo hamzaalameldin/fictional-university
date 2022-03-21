76565767a7a9744ab30e6e223febce0edda443c8
Hamza Alameldin <hamzaihab6565@gmail.com>
2022-03-21 00:43:32 -0400

    used "pageBanner" to reduce duplicate code

--- wp-content/themes/fictional-university-theme/archive-program.php
+++ wp-content/themes/fictional-university-theme/archive-program.php
@@ -1,22 +1,13 @@
 <?php
 
 get_header();
-
+pageBanner(array(
+  'title' => 'All Programs',
+  'subtitle' => 'there is somthing for everone. Have a look around.',
+));
 ?>
 
-<div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php  echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">All Programs</h1>
-        <div class="page-banner__intro">
-          <p>there is somthing for everone. Have a look around.</p>
-        </div>
-      </div>
-    </div>
-
     <div class="container container--narrow page-section">
-
-
 
 <ul class= "link-list min-list">
 
@@ -31,8 +22,7 @@ get_header();
 
   </ul>
 
-<hr class="section-break">
-<p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events') ?>">Check out our past events archive.</a></p>
+
 </div>
 
 
